3572e5eba77f3771ac8fee9502e756430b891859
zahiriniamahyar <zahiriniamahyar@gmail.com>
2025-04-06 11:36:45 +0330

    making index to register a plugin

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
-export { default } from "./CustomizedTipTap.vue";
+import { Plugin } from "vue";
+import CustomizedTipTap from "./CustomizedTipTap.vue";
+
+const CustomizedTipTapPlugin: Plugin = {
+  install(app) {
+    app.component("customized-tiptap", CustomizedTipTap);
+  },
+};
 
 export type {
   CustomizedTipTapProps,
   TiptapEditorCustomClasses,
   TiptapExtensionName,
 } from "./types/CustomizedTipTapProps";
+
+export default CustomizedTipTapPlugin;
