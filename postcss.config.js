f5d75f8810001141706327079472fde82b8c3f25
zahiriniamahyar <zahiriniamahyar@gmail.com>
2025-04-07 13:17:43 +0330

    draft: reducing bundle size

--- postcss.config.js
+++ postcss.config.js
@@ -2,5 +2,23 @@ export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    ...(process.env.VITE_BUILD_TARGET === "production"
+      ? {
+          cssnano: {
+            preset: [
+              'default',
+              {
+                discardComments: { removeAll: true },
+                normalizeWhitespace: true,
+                minifyFontValues: true,
+                colormin: true,
+                mergeLonghand: true,
+                reduceIdents: true,
+                mergeRules: true
+              }
+            ]
+          }
+        }
+      : {}),
   },
-}
+};
